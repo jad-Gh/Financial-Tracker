441a68089eaf55ac5683a33758c3ae0a2a17bbd2
jad-gh <jadghalayini1999@gmail.com>
2022-07-03 09:37:28 +0300

    Account repository

--- src/main/java/com/example/financialtracker/FinancialAccount/FinancialAccountRepository.java
+++ src/main/java/com/example/financialtracker/FinancialAccount/FinancialAccountRepository.java
@@ -1,4 +1,9 @@
 package com.example.financialtracker.FinancialAccount;
 
-public interface FinancialAccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FinancialAccountRepository extends JpaRepository<FinancialAccount,Long> {
+
 }
